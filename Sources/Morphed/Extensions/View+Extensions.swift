b1a0e4398f08514741d4aa20cb29056b0be71c6a
KrLite <wang070621@foxmail.com>
2025-01-18 22:41:29 +0800

    ✨ Filling up view modifiers

--- Sources/Morphed/Extensions/View+Extensions.swift
+++ Sources/Morphed/Extensions/View+Extensions.swift
@@ -14,12 +14,13 @@ public extension View {
     ///   - blurRadius: the maximum radius of the blur effect.
     ///   The final blur radius of every pixel will dynamically adjust based on the mask, where *full white* returns the maximum radius and *full black* returns zero.
     ///   - insets: the ``MorphedInsets`` that defines the insets of the blur effect.
+    ///   - isActive: whether the blur effect is actively applied onto the content.
     ///   - mask: the mask that configures the strength of the blur.
     @ViewBuilder func morphed<Mask: View>(
-        blurRadius: CGFloat = 50, insets: MorphedInsets = .init(),
+        blurRadius: CGFloat = 50, insets: MorphedInsets = .init(), isActive: Bool = true,
         @ViewBuilder mask: @escaping () -> Mask
     ) -> some View {
-        modifier(MorphedViewModifier(blurRadius: blurRadius, insets: insets, mask: mask))
+        modifier(MorphedViewModifier(blurRadius: blurRadius, insets: insets, isActive: isActive, mask: mask))
     }
 
     /// Applies a progressive blur effect with a simple linear gradient.
@@ -29,11 +30,12 @@ public extension View {
     ///   The final blur radius of every pixel will dynamically adjust based on the mask, where *full white* returns the maximum radius and *full black* returns zero.
     ///   - insets: the ``MorphedInsets`` that defines the insets of the blur effect.
     ///   - linearGradient: the linear gradient mask that configures the strength of the blur.
+    ///   - isActive: whether the blur effect is actively applied onto the content.
     @ViewBuilder func morphed(
-        blurRadius: CGFloat = 50, insets: MorphedInsets = .init(),
+        blurRadius: CGFloat = 50, insets: MorphedInsets = .init(), isActive: Bool = true,
         _ linearGradient: LinearGradient
     ) -> some View {
-        modifier(MorphedViewModifier(blurRadius: blurRadius, insets: insets) {
+        modifier(MorphedViewModifier(blurRadius: blurRadius, insets: insets, isActive: isActive) {
             linearGradient
                 .frame(width: 1024, height: 1024)
         })
--- Sources/Morphed/View Modifiers/MorphedViewModifier.swift
+++ Sources/Morphed/View Modifiers/MorphedViewModifier.swift
@@ -10,11 +10,12 @@ import SwiftUI
 public struct MorphedViewModifier<Mask>: ViewModifier where Mask: View {
     public var blurRadius: CGFloat = 50
     public var insets: MorphedInsets = .init()
+    public var isActive: Bool = true
 
     @ViewBuilder public var mask: () -> Mask
 
     @ViewBuilder public func body(content: Content) -> some View {
-        MorphedView(blurRadius: blurRadius, insets: insets) {
+        MorphedView(blurRadius: blurRadius, insets: insets, isActive: isActive) {
             content
         } mask: {
             mask()
