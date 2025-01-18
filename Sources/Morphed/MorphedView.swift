5ed6e46cd4bbcbb2bf3d2524cabfc63aa2a0bd95
KrLite <wang070621@foxmail.com>
2025-01-18 22:39:59 +0800

    ✨ `isActive` support

--- Sources/Morphed/MorphedView.swift
+++ Sources/Morphed/MorphedView.swift
@@ -13,6 +13,7 @@ import SwiftUI
 public struct MorphedView<Content, Mask>: NSViewRepresentable where Content: View, Mask: View {
     public let blurRadius: CGFloat
     public let insets: MorphedInsets
+    let isActive: Bool
 
     @ViewBuilder public var content: () -> Content
     @ViewBuilder public var mask: () -> Mask
@@ -23,16 +24,19 @@ public struct MorphedView<Content, Mask>: NSViewRepresentable where Content: Vie
     ///   - blurRadius: the maximum radius of the blur effect.
     ///   The final blur radius of every pixel will dynamically adjust based on the mask, where *full white* returns the maximum radius and *full black* returns zero.
     ///   - insets: the ``MorphedInsets`` that defines the insets of the blur effect.
+    ///   - isActive: whether the blur effect is actively applied onto the content.
     ///   - content: the content to apply the blur effect to.
     ///   - mask: the mask that configures the strength of the blur.
     public init(
         blurRadius: CGFloat = 50,
         insets: MorphedInsets = .init(),
+        isActive: Bool = true,
         @ViewBuilder content: @escaping () -> Content,
         @ViewBuilder mask: @escaping () -> Mask
     ) {
         self.blurRadius = blurRadius
         self.insets = insets
+        self.isActive = isActive
         self.content = content
         self.mask = mask
     }
@@ -54,38 +58,69 @@ public struct MorphedView<Content, Mask>: NSViewRepresentable where Content: Vie
             view.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
         ])
 
-        attachBlurView(to: containerView)
-
         return containerView
     }
 
-    public func updateNSView(_: NSView, context _: Context) {}
-
-    private func attachBlurView(to nsView: NSView) {
-        guard let mask = renderToCGImage(view: mask) else { return }
-        let blurView = MaskedVariableBlurView(mask: mask, blurRadius: blurRadius, insets: insets)
-
-        blurView.translatesAutoresizingMaskIntoConstraints = false
-        nsView.addSubview(blurView, positioned: .above, relativeTo: nil)
-
-        NSLayoutConstraint.activate([
-            blurView.leadingAnchor.constraint(equalTo: nsView.leadingAnchor),
-            blurView.trailingAnchor.constraint(equalTo: nsView.trailingAnchor),
-            blurView.topAnchor.constraint(equalTo: nsView.topAnchor),
-            blurView.bottomAnchor.constraint(equalTo: nsView.bottomAnchor),
-        ])
+    public func updateNSView(_ view: NSView, context: Context) {
+        if isActive {
+            context.coordinator.attachBlurView(to: view)
+        } else {
+            context.coordinator.detachBlurView()
+        }
     }
 
-    private func renderToCGImage<C: View>(view _: @escaping () -> C) -> CGImage? {
-        let renderer = ImageRenderer(content: mask())
-        return renderer.cgImage
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+
+    @MainActor public class Coordinator {
+        var parent: MorphedView<Content, Mask>
+        var blurView: MaskedVariableBlurView?
+
+        init(parent: MorphedView<Content, Mask>, blurView: MaskedVariableBlurView? = nil) {
+            self.parent = parent
+            self.blurView = blurView
+        }
+
+        func attachBlurView(to nsView: NSView) {
+            guard blurView == nil else { return }
+            guard let mask = renderToCGImage(view: parent.mask) else { return }
+            let blurView = MaskedVariableBlurView(mask: mask, blurRadius: parent.blurRadius, insets: parent.insets)
+
+            blurView.translatesAutoresizingMaskIntoConstraints = false
+            nsView.addSubview(blurView, positioned: .above, relativeTo: nil)
+
+            NSLayoutConstraint.activate([
+                blurView.leadingAnchor.constraint(equalTo: nsView.leadingAnchor),
+                blurView.trailingAnchor.constraint(equalTo: nsView.trailingAnchor),
+                blurView.topAnchor.constraint(equalTo: nsView.topAnchor),
+                blurView.bottomAnchor.constraint(equalTo: nsView.bottomAnchor),
+            ])
+
+            self.blurView = blurView
+        }
+
+        func detachBlurView() {
+            blurView?.removeFromSuperview()
+            blurView = nil
+        }
+
+        private func renderToCGImage<C: View>(view: @escaping () -> C) -> CGImage? {
+            let renderer = ImageRenderer(content: view())
+            return renderer.cgImage
+        }
     }
 }
 
+@available(macOS 15.0, *)
 #Preview {
+    @Previewable @State var isActive = true
+
+    Toggle("Active", isOn: $isActive)
+
     ZStack {
         Color.accentColor
-        MorphedView(insets: .init(bottom: .fixed(length: 64).mirrored)) {
+        MorphedView(insets: .init(bottom: .fixed(length: 64).mirrored), isActive: isActive) {
             ScrollView {
                 LinearGradient(colors: [.red, .yellow, .green, .blue, .purple], startPoint: .top, endPoint: .bottom)
                     .frame(height: 1000)
